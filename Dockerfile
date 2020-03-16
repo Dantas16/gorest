0fc2791b82bfeb8f2fa6311c418763a3f601350b
Gutemberg <gutembergdantas16@outlook.com>
2020-03-16 16:51:21 -0300

    Alteracao docker compose network

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ ADD . /home/api
 
 WORKDIR /home/api
 
+RUN go get github.com/gorilla/mux go.mongodb.org/mongo-driver
+
 EXPOSE 8080
 
 CMD ["go","run","main.go"]
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -14,28 +18,38 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"message": "hello world"}`))
 }
 
+type infoType struct {
+	ID      int    `json:"ID"`
+	Service string `json:"alias"`
+}
+
 func info(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"Info": {"id": 01, "service": "Infos from mongo"}}`))
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://mongo:27017/"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), readpref.Primary())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	w.Write([]byte(`{"Info_mongo": {"id": 01, "service": "Infos from mongo"}}`))
 }
 
 func main() {
 	fmt.Println("Starting Restful services...")
 	fmt.Println("Using port: 8080")
-	// http.HandleFunc("/hello", home)
+
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", home).Methods(http.MethodGet)
 	r.HandleFunc("/info", info).Methods(http.MethodGet)
-	log.Fatal(http.ListenAndServe(":8080", r))
-	// err := http.ListenAndServe(":8080", nil)
-	// log.Print(err)
-	// errorHandler(err)
-}
 
-/*func errorHandler(err error) {
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-}*/
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
